15e861088f9c0e9baa7e462d6207b6b4014f9226
WangYuTing <18855030268@163.com>
2024-11-19 18:58:07 +0800

    1

--- Text_Classification/multi_label_classify_bert/model3.py
+++ Text_Classification/multi_label_classify_bert/model3.py
@@ -14,7 +14,7 @@ from transformers.models.bert import BertModel, BertConfig
 args = set_args()
 
 
-# BERT+TextCNN
+# BERT+TextCNN 
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
@@ -24,11 +24,13 @@ class Model(nn.Module):
         self.dropout = nn.Dropout(0.5)
 
         # TextCNN部分
-        filter_sizes = [2, 3, 4]   # 卷积核的尺寸
-        filter_num = 128   # 卷积核的个数
+        filter_sizes = [2, 3, 4]  # 卷积核的尺寸
+        filter_num = 128  # 卷积核的个数
         self.convs = nn.ModuleList(
-            [nn.Conv2d(1, filter_num, kernel_size=(size, self.config.hidden_size))
-             for size in filter_sizes]
+            [
+                nn.Conv2d(1, filter_num, kernel_size=(size, self.config.hidden_size))
+                for size in filter_sizes
+            ]
         )
 
         mid_size = 256
@@ -37,16 +39,16 @@ class Model(nn.Module):
             nn.BatchNorm1d(mid_size),
             nn.ReLU(),
             nn.Dropout(0.5),
-            nn.Linear(mid_size, args.num_classes)
+            nn.Linear(mid_size, args.num_classes),
         )
 
     def forward(self, input_ids, attention_mask):
         bert_output = self.bert(input_ids, attention_mask=attention_mask)
-        bert_hidden = bert_output[0]   # torch.Size([2, 256, 768])
+        bert_hidden = bert_output[0]  # torch.Size([2, 256, 768])
 
         output = self.dropout(bert_hidden)
 
-        tcnn_input = output.unsqueeze(1)   # torch.Size([2, 1, 256, 768]  # 加了一个通道
+        tcnn_input = output.unsqueeze(1)  # torch.Size([2, 1, 256, 768]  # 加了一个通道
 
         tcnn_output = [F.relu(conv(tcnn_input)).squeeze(3) for conv in self.convs]
 
@@ -55,7 +57,9 @@ class Model(nn.Module):
         # print(tcnn_output[2].size())   # torch.Size([2, 128, 253])
 
         # 对三组数据分别进行池化然后拼接
-        tcnn_output = [F.max_pool1d(item, item.size(2)).squeeze(2) for item in tcnn_output]
+        tcnn_output = [
+            F.max_pool1d(item, item.size(2)).squeeze(2) for item in tcnn_output
+        ]
         # print(tcnn_output[0].size())   # torch.Size([2, 128, 255])
         # print(tcnn_output[1].size())   # torch.Size([2, 128, 254])
         # print(tcnn_output[2].size())   # torch.Size([2, 128, 253])
